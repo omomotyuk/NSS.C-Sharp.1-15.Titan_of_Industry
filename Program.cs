d6dfcfacd90ef13dd54b8fb3068693ff15e4e1ba
Alex Momotyuk <o.a.momotyuk@gmail.com>
2019-11-26 11:17:39 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1_15.Titan_of_Industry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# NSS.C-Sharp.1-15.Titan_of_Industry
